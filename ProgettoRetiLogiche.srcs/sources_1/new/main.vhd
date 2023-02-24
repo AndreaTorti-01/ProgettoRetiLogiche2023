d222aac0c5c46bd1f9dff42b0d73b3f2f9ada49b
AndreaTorti-01 <eliandrea.yt@gmail.com>
2023-02-24 11:08:39 +0100

    initial commit

---
+++ ProgettoRetiLogiche.srcs/sources_1/new/main.vhd
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.02.2023 14:18:59
+-- Design Name: 
+-- Module Name: main - archi
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+ENTITY project_reti_logiche IS
+    PORT (
+        i_clk : IN STD_LOGIC;
+        i_rst : IN STD_LOGIC;
+        i_start : IN STD_LOGIC;
+        i_w : IN STD_LOGIC;
+        o_z0 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        o_z1 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        o_z2 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        o_z3 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        o_done : OUT STD_LOGIC;
+        o_mem_addr : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        i_mem_data : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        o_mem_we : OUT STD_LOGIC;
+        o_mem_en : OUT STD_LOGIC
+    );
+END project_reti_logiche;
+
+ARCHITECTURE archi OF project_reti_logiche IS
+
+BEGIN
+END archi;
