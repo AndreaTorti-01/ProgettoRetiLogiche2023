7b8f36b4b9a3958a77f107dd2b14112e77b104e5
Andrea Torti <eliandrea.yt@gmail.com>
2023-03-10 10:34:50 +0100

    rifatto tutto, funziona con il primo tb

--- ProgettoRetiLogiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
+++ ProgettoRetiLogiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
@@ -54,29 +54,31 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
+IKI_DLLESPEC extern void execute_37(char*, char *);
 IKI_DLLESPEC extern void execute_38(char*, char *);
 IKI_DLLESPEC extern void execute_39(char*, char *);
 IKI_DLLESPEC extern void execute_40(char*, char *);
-IKI_DLLESPEC extern void execute_41(char*, char *);
+IKI_DLLESPEC extern void execute_34(char*, char *);
+IKI_DLLESPEC extern void execute_35(char*, char *);
 IKI_DLLESPEC extern void execute_36(char*, char *);
-IKI_DLLESPEC extern void execute_37(char*, char *);
 IKI_DLLESPEC extern void execute_27(char*, char *);
 IKI_DLLESPEC extern void execute_29(char*, char *);
 IKI_DLLESPEC extern void execute_31(char*, char *);
 IKI_DLLESPEC extern void execute_33(char*, char *);
-IKI_DLLESPEC extern void execute_35(char*, char *);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-IKI_DLLESPEC extern void transaction_3(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void transaction_18(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_19(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[15] = {(funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_41, (funcp)execute_36, (funcp)execute_37, (funcp)execute_27, (funcp)execute_29, (funcp)execute_31, (funcp)execute_33, (funcp)execute_35, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_3, (funcp)transaction_4, (funcp)transaction_19};
-const int NumRelocateId= 15;
+IKI_DLLESPEC extern void transaction_20(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void transaction_21(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[17] = {(funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_34, (funcp)execute_35, (funcp)execute_36, (funcp)execute_27, (funcp)execute_29, (funcp)execute_31, (funcp)execute_33, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_4, (funcp)transaction_18, (funcp)transaction_19, (funcp)transaction_20, (funcp)transaction_21};
+const int NumRelocateId= 17;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/project_tb_behav/xsim.reloc",  (void **)funcTab, 15);
-	iki_vhdl_file_variable_register(dp + 1057264);
-	iki_vhdl_file_variable_register(dp + 1057320);
+	iki_relocate(dp, "xsim.dir/project_tb_behav/xsim.reloc",  (void **)funcTab, 17);
+	iki_vhdl_file_variable_register(dp + 1057816);
+	iki_vhdl_file_variable_register(dp + 1057872);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- old.vhd
+++ old.vhd
@@ -21,27 +21,16 @@ ENTITY project_reti_logiche IS
 END project_reti_logiche;
 
 ARCHITECTURE structural OF project_reti_logiche IS
-    COMPONENT signal_generator IS
-        PORT (
-            clk : IN STD_LOGIC;
-            rst : IN STD_LOGIC;
-            start : IN STD_LOGIC;
-            clk_read_output : OUT STD_LOGIC;
-            clk_read_address : OUT STD_LOGIC;
-            clk_done : OUT STD_LOGIC;
-            clk_en : OUT STD_LOGIC;
-            clk_mega_mux : OUT STD_LOGIC
-        );
-    END COMPONENT;
 
-    COMPONENT serial_to_parallel IS
-        GENERIC (N : INTEGER);
+    COMPONENT output_selector
         PORT (
+            w : IN STD_LOGIC;
+            start : IN STD_LOGIC;
             clk : IN STD_LOGIC;
             rst : IN STD_LOGIC;
-            done : IN STD_LOGIC;
-            input : IN STD_LOGIC;
-            output : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0)
+            read_address_en : OUT STD_LOGIC;
+            out1 : OUT STD_LOGIC;
+            out2 : OUT STD_LOGIC
         );
     END COMPONENT;
 
@@ -49,178 +38,161 @@ ARCHITECTURE structural OF project_reti_logiche IS
         PORT (
             clk : IN STD_LOGIC;
             rst : IN STD_LOGIC;
-            done : IN STD_LOGIC;
             do : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-            out_port : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+            out1 : IN STD_LOGIC;
+            out2 : IN STD_LOGIC;
+            read_address_en : IN STD_LOGIC;
             z0 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
             z1 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
             z2 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
             z3 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
         );
     END COMPONENT;
-    SIGNAL s_clk_read_output : STD_LOGIC;
-    SIGNAL s_clk_read_address : STD_LOGIC;
-    SIGNAL s_clk_done : STD_LOGIC;
 
-    SIGNAL s_clk_mega_mux : STD_LOGIC;
-    SIGNAL s_out_port : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    SIGNAL s_out_address : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    COMPONENT address_reader IS
+        PORT (
+            rst : IN STD_LOGIC;
+            w : IN STD_LOGIC;
+            read_address_en : IN STD_LOGIC;
+            address : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            en : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
+    COMPONENT two_cycles_delay IS
+        PORT (
+            clk : IN STD_LOGIC;
+            rst : IN STD_LOGIC;
+            input : IN STD_LOGIC;
+            output : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    COMPONENT done_generator IS
+        PORT (
+            rst : IN STD_LOGIC;
+            start : IN STD_LOGIC;
+            done : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    SIGNAL s_done : STD_LOGIC;
+    SIGNAL s_read_address_en : STD_LOGIC;
+    SIGNAL s_out1 : STD_LOGIC;
+    SIGNAL s_out2 : STD_LOGIC;
+    SIGNAL s_en : STD_LOGIC;
 BEGIN
-    signal_generator0 : signal_generator
+
+    output_selector_0 : output_selector
     PORT MAP(
+        w => i_w,
+        start => i_start,
         clk => i_clk,
         rst => i_rst,
-        start => i_start,
-        clk_read_output => s_clk_read_output,
-        clk_read_address => s_clk_read_address,
-        clk_done => s_clk_done,
-        clk_en => o_mem_en,
-        clk_mega_mux => s_clk_mega_mux
-
-    );
-    serial_to_parallel_port : serial_to_parallel
-    GENERIC MAP(n => 2)
-    PORT MAP(
-        clk => s_clk_read_output,
-        rst => i_rst,
-        done => s_clk_done,
-        input => i_w,
-        output => s_out_port
-
-    );
-
-    serial_to_parallel_address : serial_to_parallel
-    GENERIC MAP(n => 16)
-    PORT MAP(
-        clk => s_clk_read_address,
-        rst => i_rst,
-        done => s_clk_done,
-        input => i_w,
-        output => s_out_address
+        read_address_en => s_read_address_en,
+        out1 => s_out1,
+        out2 => s_out2
     );
 
     mega_mux_0 : mega_mux
     PORT MAP(
-        clk => s_clk_mega_mux,
+        clk => i_clk,
         rst => i_rst,
-        done => s_clk_done,
         do => i_mem_data,
-        out_port => s_out_port,
+        out1 => s_out1,
+        out2 => s_out2,
+        read_address_en => s_read_address_en,
         z0 => o_z0,
         z1 => o_z1,
         z2 => o_z2,
         z3 => o_z3
     );
+
+    address_reader_0 : address_reader
+    PORT MAP(
+        rst => i_rst,
+        w => i_w,
+        en => s_en,
+        read_address_en => s_read_address_en,
+        address => o_mem_addr
+    );
+
+    done_generator_0 : done_generator
+    PORT MAP(
+        rst => i_rst,
+        start => i_start,
+        done => s_done
+    );
+
+    two_cycles_delay_0 : two_cycles_delay
+    PORT MAP(
+        clk => i_clk,
+        rst => i_rst,
+        input => s_done,
+        output => o_done
+    );
+
+    o_mem_en <= s_en;
     o_mem_we <= '0';
-    o_mem_addr <= s_out_address;
-    o_done <= s_clk_done;
 END structural;
-LIBRARY ieee;
+
+LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
-ENTITY signal_generator IS
+ENTITY output_selector IS
     PORT (
+        w : IN STD_LOGIC;
+        start : IN STD_LOGIC;
         clk : IN STD_LOGIC;
         rst : IN STD_LOGIC;
-        start : IN STD_LOGIC;
-        clk_read_output : OUT STD_LOGIC;
-        clk_read_address : OUT STD_LOGIC;
-        clk_done : OUT STD_LOGIC;
-        clk_en : OUT STD_LOGIC;
-        clk_mega_mux : OUT STD_LOGIC
+        read_address_en : OUT STD_LOGIC;
+        out1 : OUT STD_LOGIC;
+        out2 : OUT STD_LOGIC
     );
-END signal_generator;
+END output_selector;
 
-ARCHITECTURE behavioral OF signal_generator IS
-    TYPE state_type IS (a, b, c, clk_wait, d, e);
+ARCHITECTURE behavioral OF output_selector IS
+    TYPE state_type IS (a, b, c);
     SIGNAL state : state_type;
+    SIGNAL t_out1 : STD_LOGIC;
 BEGIN
     PROCESS (clk, rst)
-        VARIABLE v_clk_read_output : STD_LOGIC;
-        VARIABLE v_clk_read_address : STD_LOGIC;
-        VARIABLE v_clk_done : STD_LOGIC;
-        VARIABLE v_clk_en : STD_LOGIC;
-        VARIABLE v_clk_mega_mux : STD_LOGIC;
+        VARIABLE t_read_address_en : STD_LOGIC;
     BEGIN
         IF (rst = '1') THEN
-            state <= a;
-            v_clk_read_output := '0';
-            v_clk_read_address := '0';
-            v_clk_done := '0';
-            v_clk_en := '0';
-
+            state <= A;
+            out1 <= '0';
+            out2 <= '0';
+            t_read_address_en := '0';
         ELSIF (rising_edge(clk)) THEN
+            IF state = c THEN
+                t_read_address_en := '1';
+            END IF;
             CASE state IS
                 WHEN a =>
-                    v_clk_done := '0';
-                    IF start = '1' THEN
+                    IF start = '0' THEN
+                        state <= a;
+                    ELSIF start = '1' THEN
                         state <= b;
-                        v_clk_read_output := '1';
+                        t_out1 <= w;
                     END IF;
                 WHEN b =>
-                    IF start = '1' THEN
+                    IF start = '0' THEN
+                        state <= b;
+                    ELSIF start = '1' THEN
                         state <= c;
-                        v_clk_read_output := '1';
+                        out1 <= t_out1;
+                        out2 <= w;
                     END IF;
                 WHEN c =>
-                    v_clk_read_output := '0';
-                    IF start = '1' THEN
-                        v_clk_read_address := '1';
-                    ELSE
-                        v_clk_en := '1'; --va in o_mem_en
-                        v_clk_read_address := '0';
-                        state <= clk_wait;
+                    IF start = '0' THEN
+                        state <= a;
+                    ELSIF start = '1' THEN
+                        state <= c;
                     END IF;
-                WHEN clk_wait =>
-                    v_clk_en := '0';
-                    state <= d;
-                WHEN d =>
-                    state <= e;
-                    v_clk_mega_mux := '1'; --va in done e in serial to parallel per reset
-                WHEN e =>
-                    v_clk_mega_mux := '0';
-                    v_clk_done := '1';
-                    state <= a;
-                WHEN OTHERS => NULL;
             END CASE;
         END IF;
-        clk_en <= v_clk_en;
-        clk_read_output <= v_clk_read_output AND clk;
-        clk_read_address <= v_clk_read_address AND clk;
-        clk_done <= v_clk_done AND clk;
-        clk_mega_mux <= v_clk_mega_mux AND clk;
-    END PROCESS;
-END behavioral;
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-
-ENTITY serial_to_parallel IS
-    GENERIC (
-        N : INTEGER
-    );
-    PORT (
-        clk : IN STD_LOGIC;
-        rst : IN STD_LOGIC;
-        done : IN STD_LOGIC;
-        input : IN STD_LOGIC;
-        output : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0)
-    );
-END serial_to_parallel;
-
-ARCHITECTURE behavioral OF serial_to_parallel IS
-    SIGNAL t_output : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-BEGIN
-    PROCESS (clk, rst, done)
-    BEGIN
-        IF (rst = '1' OR done = '1') THEN
-            t_output <= (OTHERS => '0');
-            output <= (OTHERS => '0');
-        ELSIF (rising_edge(clk)) THEN
-            t_output <= t_output (N - 2 DOWNTO 0) & input;
-            output <= t_output (N - 2 DOWNTO 0) & input;
-        END IF;
-
+        read_address_en <= t_read_address_en AND start AND clk;
     END PROCESS;
 END behavioral;
 
@@ -231,9 +203,10 @@ ENTITY mega_mux IS
     PORT (
         clk : IN STD_LOGIC;
         rst : IN STD_LOGIC;
-        done : IN STD_LOGIC;
         do : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-        out_port : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        out1 : IN STD_LOGIC;
+        out2 : IN STD_LOGIC;
+        read_address_en : IN STD_LOGIC;
         z0 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
         z2 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
         z1 : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
@@ -242,46 +215,113 @@ ENTITY mega_mux IS
 END mega_mux;
 
 ARCHITECTURE behavioral OF mega_mux IS
-    SIGNAL t_z0 : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    SIGNAL t_z1 : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    SIGNAL t_z2 : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    SIGNAL t_z3 : STD_LOGIC_VECTOR (7 DOWNTO 0);
 BEGIN
-    PROCESS (clk, rst, done)
+    PROCESS (clk, rst)
     BEGIN
         IF (rst = '1') THEN
             z0 <= (OTHERS => '0');
             z1 <= (OTHERS => '0');
             z2 <= (OTHERS => '0');
             z3 <= (OTHERS => '0');
-            t_z0 <= (OTHERS => '0');
-            t_z1 <= (OTHERS => '0');
-            t_z2 <= (OTHERS => '0');
-            t_z3 <= (OTHERS => '0');
         ELSIF (rising_edge(clk)) THEN
-            CASE out_port IS
-                WHEN "00" =>
-                    t_z0 <= do;
-                WHEN "01" =>
-                    t_z1 <= do;
-                WHEN "10" =>
-                    t_z2 <= do;
-                WHEN "11" =>
-                    t_z3 <= do;
-                WHEN OTHERS => NULL;
-            END CASE;
+            IF (read_address_en = '1') THEN
+                IF (out1 = '0' AND out2 = '0') THEN
+                    z0 <= do;
+                ELSIF (out1 = '0' AND out2 = '1') THEN
+                    z1 <= do;
+                ELSIF (out1 = '1' AND out2 = '0') THEN
+                    z2 <= do;
+                ELSIF (out1 = '1' AND out2 = '1') THEN
+                    z3 <= do;
+                END IF;
+            END IF;
         END IF;
-        IF (rising_edge(done)) THEN
-            z0 <= t_z0;
-            z1 <= t_z1;
-            z2 <= t_z2;
-            z3 <= t_z3;
+    END PROCESS;
+END behavioral;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY address_reader IS
+    PORT (
+        rst : IN STD_LOGIC;
+        w : IN STD_LOGIC;
+        read_address_en : IN STD_LOGIC;
+        address : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+        en : OUT STD_LOGIC
+    );
+END address_reader;
+
+ARCHITECTURE behavioral OF address_reader IS
+    SIGNAL t_address : STD_LOGIC_VECTOR (15 DOWNTO 0);
+    SIGNAL m_nonzero : STD_LOGIC;
+BEGIN
+    PROCESS (read_address_en, rst)
+    BEGIN
+        IF (rst = '1') THEN
+            en <= '0';
+            address <= (OTHERS => '0');
+            t_address <= (OTHERS => '0');
+            m_nonzero <= '0';
+        ELSIF (rising_edge(read_address_en)) THEN
+            t_address <= t_address(address'HIGH - 1 DOWNTO address'LOW) & w;
+            address <= t_address(address'HIGH - 1 DOWNTO address'LOW) & w;
+            m_nonzero <= '1';
+            IF (m_nonzero = '1') THEN
+                en <= '1';
+            END IF;
         END IF;
-        IF (falling_edge(done)) THEN
-            z0 <= (OTHERS => '0');
-            z1 <= (OTHERS => '0');
-            z2 <= (OTHERS => '0');
-            z3 <= (OTHERS => '0');
+    END PROCESS;
+END behavioral;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY two_cycles_delay IS
+    PORT (
+        clk : IN STD_LOGIC;
+        rst : IN STD_LOGIC;
+        input : IN STD_LOGIC;
+        output : OUT STD_LOGIC
+    );
+END ENTITY two_cycles_delay;
+
+ARCHITECTURE Behavioral OF two_cycles_delay IS
+
+    SIGNAL rd_en_d1, rd_en_d2 : STD_LOGIC;
+BEGIN
+    PROCESS (clk, rst)
+    BEGIN
+        IF (rst = '1') THEN
+            rd_en_d1 <= '0';
+            rd_en_d2 <= '0';
+        ELSIF (rising_edge(clk)) THEN
+            output <= rd_en_d2;
+            rd_en_d2 <= rd_en_d1;
+            rd_en_d1 <= input;
+        END IF;
+    END PROCESS;
+END ARCHITECTURE Behavioral;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY done_generator IS
+    PORT (
+        rst : IN STD_LOGIC;
+        start : IN STD_LOGIC;
+        done : OUT STD_LOGIC
+    );
+END done_generator;
+
+ARCHITECTURE behavioral OF done_generator IS
+BEGIN
+    PROCESS (start, rst)
+    BEGIN
+        IF (rst = '1') THEN
+            done <= '0';
+        ELSIF (falling_edge(start)) THEN
+            done <= '1';
         END IF;
     END PROCESS;
 END behavioral;
